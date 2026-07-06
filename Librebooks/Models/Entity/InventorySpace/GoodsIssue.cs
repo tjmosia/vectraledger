d0ca744e5287f731ee715e3d66e602edae601188
Mosia Thabo <tjmosia@outlook.com>
2026-07-06 07:59:09 +0200

    V2607060759

--- Librebooks/Models/Entity/InventorySpace/GoodsIssue.cs
+++ Librebooks/Models/Entity/InventorySpace/GoodsIssue.cs
@@ -1,18 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Librebooks.Extensions.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Librebooks.Models.Entity.InventorySpace;
 
 [Table(nameof(GoodsIssue))]
-public class GoodsIssue
+public class GoodsIssue(): VersionedEntityBase()
 {
+	[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 	public virtual int Id { get; set; }
 	public virtual DateTime Date { get; set;  }
 	public virtual DateTime DateApproved { get; set; }
 	public virtual string? Number { get; set; }
-	public virtual string? Description { get; set;  }
-	public virtual string? Reference { get; set; }
+    public virtual string? Reference { get; set; }
+    public virtual string? Description { get; set;  }
 	public virtual int CompanyId { get; set; }
 	public virtual bool Recorded { get; set;  }
 
@@ -22,11 +24,10 @@ public class GoodsIssue
 	{
 		modelBuilder.Entity<GoodsIssue>( options =>
 		{
-			options.HasKey( x => x.Id );
-            options.Property(x => x.Id).UseIdentityColumn();
             options.HasIndex(p => new { p.CompanyId, p.Id }).IsClustered();
-
 			options.Property(p => p.Description).HasMaxLength(255);
+			options.Property(p => p.Reference).HasMaxLength(20);
+			options.Property(p => p.Number).HasMaxLength(75);
 		});
 	}
 }
--- Librebooks/Models/Entity/InventorySpace/GoodsIssueItem.cs
+++ Librebooks/Models/Entity/InventorySpace/GoodsIssueItem.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Librebooks.Core.Constants;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,11 +11,21 @@ public class GoodsIssueItem
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public virtual int Id { get; set; }
     public virtual int IssueId { get; set;  }
+    public virtual int ItemId { get; set; }
+    public virtual string? Unit { get; set; }
+    public virtual string? Code { get; set; }
+    public virtual string? Description { get; set;  }
+    public virtual decimal Quantity { get; set;  }
+    public virtual int WarehouseId { get; set; }
 
     public static void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<GoodsIssueItem>( entity =>
         {
+            entity.Property(p=>p.Unit).HasMaxLength(20);
+            entity.Property(p=>p.Description).HasMaxLength(20).IsRequired();
+            entity.Property(p=>p.Code).HasMaxLength(20);
+            entity.Property(p => p.Quantity).HasColumnType(ColumnTypes.NUMBER);
 
         });
     }
